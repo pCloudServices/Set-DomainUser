a06188f115f6604bc1af3fe4f864c0864fde4263
hjbotha-cybr <henning.botha@cyberark.com>
2022-06-22 17:08:21 +0100

    add function to handle Invoke-RestMethod errors

--- Set-DomainUser.ps1
+++ Set-DomainUser.ps1
@@ -103,6 +103,49 @@ param(
 
 #Functions
 
+Function Get-RestMethodError {
+    <# Invoke-RestMethod can have several different possible results
+    Connection failure: The connection error will be contained in $_.Exception.Message
+    Successful connection:
+        200 result
+            The request was successful: This function should not be called in this case
+        Some other result
+            JSON data returned: We should return the JSON, contained in $_.ErrorDetails.Message
+            non-JSON data returned: We should return the non-JSON data, contained in $_.ErrorDetails.Message
+
+    TODO: Convert other functions to use this function to return useful errors.
+    This is lower priority as, if New-ConnectionToRestAPI and Test-PvwaToken succeed,
+    others are unlikely to have issues.
+
+    NOTE: This function should only be called if Invoke-RestMethod fails.
+    Do not use it to catch errors from any other commands.
+    #>
+    param(
+        [Parameter(Mandatory = $true)][System.Management.Automation.ErrorRecord]$ErrorRecord
+    )
+    If ($ErrorRecord.ErrorDetails.Message) {
+        # If the connection was successful but the server returned a non-200 result, Invoke-RestMethod will treat it as an error.
+        # But if it's valid JSON, we'd rather know what the server said than what Invoke-RestMethod thought about it.
+        try {
+            return $ErrorRecord.ErrorDetails.Message | ConvertFrom-Json
+        }
+        catch {
+            # Doesn't seem to have been valid JSON, so we'll construct our own object with the server's response (which is in the ErrorDetails property)
+            return @{
+                ErrorCode    = "Unknown"
+                ErrorMessage = $ErrorRecord.ErrorDetails.Message
+            }
+        }
+    }
+    else {
+        # If there's no ErrorDetails.Message property, likely the connection failed entirely, so we'll return something based on the Exception.Message instead, which will be what Invoke-RestMethod thought.
+        return @{
+            ErrorCode    = "Unknown"
+            ErrorMessage = $ErrorRecord.Exception.Message
+        }
+    }
+}
+
 Function Get-DomainDnsName {
     if ($env:USERDNSDOMAIN) {
         return $env:USERDNSDOMAIN
