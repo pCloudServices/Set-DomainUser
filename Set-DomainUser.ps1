ae91bc50ca67ea16cd254dc1e87e9784e27b6849
hjbotha-cybr <hbotha@paloaltonetworks.com>
2026-08-14 10:59:06 +0200

    name and version updates

--- Set-DomainUser.ps1
+++ Set-DomainUser.ps1
@@ -42,8 +42,8 @@
  Do not update Local Security Policy to allow PSM users to log on with Remote Desktop
 .PARAMETER SkipAddingUsersToRduGroup
  Do not add PSM users to the Remote Desktop Users group
-.VERSION 14.5
-.AUTHOR CyberArk
+.VERSION 15.0.0
+.AUTHOR Palo Alto Networks Idira Professional Services
 #>
 
 [CmdletBinding()]
